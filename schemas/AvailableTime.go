db01122350cd64b17a41be2ac5776c1a42729dbf
Cornelia Olofsson <guscornol@student.gu.se>
2023-12-06 14:42:18 +0100

    #3 (completed) remove unneeded imported validator

--- schemas/AvailableTime.go
+++ schemas/AvailableTime.go
@@ -1,24 +1,12 @@
 package schemas
 
 import (
-	"fmt"
-
-	"github.com/go-playground/validator"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type AvailableTime struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	Dentist_id primitive.ObjectID `bson:"Dentist_id" validate:"required,hexadecimal,len=24"`
-	Start_time primitive.DateTime `bson:"Start_time" validate:"required"`
-	End_time   primitive.DateTime `bson:"End_time" validate:"required,gtefield=StartTime"`
-}
-
-var validate = validator.New()
-
-func (at *AvailableTime) Validate() error {
-	if err := validate.Struct(at); err != nil {
-		return fmt.Errorf("validation error: %v", err)
-	}
-	return nil
+	Dentist_id primitive.ObjectID `bson:"Dentist_id"`
+	Start_time primitive.DateTime `bson:"Start_time"`
+	End_time   primitive.DateTime `bson:"End_time"`
 }
