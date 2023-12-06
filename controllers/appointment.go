bbf3154945f0d5fcb4cf18b3d5a3a1d5862ae93b
Cornelia Olofsson <guscornol@student.gu.se>
2023-12-06 13:42:30 +0100

    #2 (ongoing) handle payload malformation

--- controllers/appointment.go
+++ controllers/appointment.go
@@ -5,7 +5,7 @@ import (
 	"Group20/appointment-service/schemas"
 	"context"
 	"encoding/json"
-    "fmt"
+	"fmt"
 
 	//"encoding/json"
 
@@ -15,222 +15,244 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func InitialiseAppointment (client mqtt.Client) {
+func InitialiseAppointment(client mqtt.Client) {
 
-//CREATE
-    tokenCancel := client.Subscribe("grp20/req/appointment/delete", byte(0), func(c mqtt.Client, m mqtt.Message){
+	//CREATE
+	tokenCancel := client.Subscribe("grp20/req/appointment/delete", byte(0), func(c mqtt.Client, m mqtt.Message) {
 
 		var payload schemas.Appointment
-        err := json.Unmarshal(m.Payload(), &payload)
-        if err != nil {
-            panic(err)
-        }
-        go CancelAppointment(payload.ID, client)
-    })
-    if tokenCancel.Error() != nil {
-        panic(tokenCancel.Error())
-    }
+		err := json.Unmarshal(m.Payload(), &payload)
+		if err != nil {
+			//send mqtt message 400 bad request
+			fmt.Printf("400 - Bad request")
+		} else {
+			go CancelAppointment(payload.ID, client)
+		}
+	})
+	if tokenCancel.Error() != nil {
+		panic(tokenCancel.Error())
+	}
 
-    tokenCreate := client.Subscribe("grp20/req/appointment/create", byte(0), func(c mqtt.Client, m mqtt.Message) {
+	tokenCreate := client.Subscribe("grp20/req/appointment/create", byte(0), func(c mqtt.Client, m mqtt.Message) {
 
-        var payload schemas.Appointment
-        err := json.Unmarshal(m.Payload(), &payload)
-        if err != nil {
-            panic(err)
-        }
-        go CreateAppointment(payload, client)
+		var payload schemas.Appointment
+		err := json.Unmarshal(m.Payload(), &payload)
+		if err != nil {
+			//send mqtt message 400 bad request
+			fmt.Printf("400 - Bad request")
+		} else {
+			go CreateAppointment(payload, client)
+		}
+	})
+	if tokenCreate.Error() != nil {
+		panic(tokenCreate.Error())
+	}
 
-    })
-    if tokenCreate.Error() != nil {
-        panic(tokenCreate.Error())
-    }
+	tokenGetAllForUser := client.Subscribe("grp20/req/appointment/get", byte(0), func(c mqtt.Client, m mqtt.Message) {
 
-    tokenGetAllForUser := client.Subscribe("grp20/req/appointment/get", byte(0), func(c mqtt.Client, m mqtt.Message){
-        
-        var payload schemas.Appointment
-        err := json.Unmarshal(m.Payload(), &payload)
-        if err != nil {
-            panic(err)
-        }
-        go GetAllForUser(payload.Patient_id, client)
-    })
-    if tokenGetAllForUser.Error() != nil {
-        panic(tokenGetAllForUser.Error())
-    }
+		var payload schemas.Appointment
+		err := json.Unmarshal(m.Payload(), &payload)
+		if err != nil {
+			//send mqtt message 400 bad request
+			fmt.Printf("400 - Bad request")
+		} else {
+			go GetAllForUser(payload.Patient_id, client)
+		}
+	})
+	if tokenGetAllForUser.Error() != nil {
+		panic(tokenGetAllForUser.Error())
+	}
 
-    tokenDelete := client.Subscribe("appointmentservice/internal/delete", byte(0), func(c mqtt.Client, m mqtt.Message) {
+	tokenDelete := client.Subscribe("appointmentservice/internal/delete", byte(0), func(c mqtt.Client, m mqtt.Message) {
 
-        var payload schemas.Appointment
-        err := json.Unmarshal(m.Payload(), &payload)
-        if err != nil {
-            panic(err)
-        }
-        go DeleteAppointment(payload.ID, client)
-    })
-    if tokenDelete.Error() != nil {
-        panic(tokenDelete.Error())
-    }
-
+		var payload schemas.Appointment
+		err := json.Unmarshal(m.Payload(), &payload)
+		if err != nil {
+			//send mqtt message 400 bad request or ignore due to internal?
+			fmt.Printf("400 - Bad request")
+		} else {
+			go DeleteAppointment(payload.ID, client)
+		}
+	})
+	if tokenDelete.Error() != nil {
+		panic(tokenDelete.Error())
+	}
 
 }
 
 func DeleteAppointment(id primitive.ObjectID, client mqtt.Client) bool {
-    var message string
-    var code string
-    var returnVal bool
+	var message string
+	var code string
+	var returnVal bool
 
-    col := getAppointmentCollection()
-    filter := bson.M{"_id": id}
-    result, err := col.DeleteOne(context.TODO(), filter)
-    if err != nil{
-        panic(err)
-    }
+	col := getAppointmentCollection()
+	filter := bson.M{"_id": id}
+	result, err := col.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
 
-    if result.DeletedCount == 1 {
+	if result.DeletedCount == 1 {
 
-        message = `{"message": "User deleted"`
-        code = "200"
+		message = `{"message": "User deleted"`
+		code = "200"
 
-        message = AddCodeStringJson(message, code)
-    } else{
-        message = `{"message": "User not found"`
-        code = "404"
-        message = AddCodeStringJson(message, code)
-    }
+		message = AddCodeStringJson(message, code)
+	} else {
+		message = `{"message": "User not found"`
+		code = "404"
+		message = AddCodeStringJson(message, code)
+	}
 
-    client.Publish("grp20/res/dentist/delete", byte(0), false, message)
-    returnVal = true
-    return returnVal
+	client.Publish("grp20/res/dentist/delete", byte(0), false, message)
+	returnVal = true
+	return returnVal
 }
 
 func GetAllForUser(id primitive.ObjectID, client mqtt.Client) bool {
 
-    var message string
-    var code string
-    var returnVal bool
+	var message string
+	var code string
+	var returnVal bool
 
-    col := getAppointmentCollection()
-    filter := bson.M{"patient_id": id}
+	col := getAppointmentCollection()
+	filter := bson.M{"patient_id": id}
 
-    cursor, err := col.Find(context.TODO(), filter)
+	cursor, err := col.Find(context.TODO(), filter)
 
-    if err != nil {
-        message = `{"message": "Error"`
-        code = "500"
-    }
+	if err != nil {
+		message = `{"message": "Error"`
+		code = "500"
+	}
 
-    defer cursor.Close(context.TODO())
+	defer cursor.Close(context.TODO())
 
-    var appointments []schemas.Appointment
+	var appointments []schemas.Appointment
 
-    for cursor.Next(context.TODO()) {
-        var appointment schemas.Appointment
+	for cursor.Next(context.TODO()) {
+		var appointment schemas.Appointment
 
-        if err := cursor.Decode(&appointment); err != nil {
-            message = `{"message": "Error"`
-            code = "500"
-            panic(err)
-        }
-        
-        appointments = append(appointments, appointment)
-    }
-    jsonData, err := json.Marshal(appointments)
-    fmt.Printf(string(jsonData))
-    returnVal = true
-    
-    code = "200"
-    message = AddCodeStringJson(string(jsonData), code)
+		if err := cursor.Decode(&appointment); err != nil {
+			message = `{"message": "Error"`
+			code = "500"
+			panic(err)
+		}
 
-    client.Publish("grp20/res/appointment/get", 0, false, message)
+		appointments = append(appointments, appointment)
+	}
+	jsonData, err := json.Marshal(appointments)
+	fmt.Printf(string(jsonData))
+	returnVal = true
 
-    return returnVal
+	code = "200"
+	message = AddCodeStringJson(string(jsonData), code)
+
+	client.Publish("grp20/res/appointment/get", 0, false, message)
+
+	return returnVal
 }
 
-func CreateAppointment(payload schemas.Appointment, client mqtt.Client) bool{
-    var message string
-    var code string
-    var returnVal bool
+func CreateAppointment(payload schemas.Appointment, client mqtt.Client) bool {
+	var message string
+	var code string
+	var returnVal bool
 
-    col := getAppointmentCollection()
+	col := getAppointmentCollection()
 
-    result, err := col.InsertOne(context.TODO(), payload)
-    _ = result
+	result, err := col.InsertOne(context.TODO(), payload)
+	_ = result
 
-    if err != nil {
-        code = "500"
-        message = `{"message": "Appointmend could not be created"`
-        client.Publish("grp20/res/appointment/create", 0, false, message)
-        panic(err)
-    }
+	if err != nil {
+		code = "500"
+		message = `{"message": "Appointmend could not be created"`
+		client.Publish("grp20/res/appointment/create", 0, false, message)
+		panic(err)
+	}
 
-    message = `{"message": "Appointment booked"`
-    code = "200"
-    returnVal = true
+	message = `{"message": "Appointment booked"`
+	code = "200"
+	returnVal = true
 
-    message = AddCodeStringJson(message, code)
+	message = AddCodeStringJson(message, code)
 
-    client.Publish("grp20/res/appointment/create", 0, false, message)
-    return returnVal
+	client.Publish("grp20/res/appointment/create", 0, false, message)
+	return returnVal
 }
 
-func CancelAppointment(id primitive.ObjectID, client mqtt.Client) bool{
-    var message string
-    var code string
-    var returnVal bool
-    appointment := &schemas.Appointment{}
+func CancelAppointment(id primitive.ObjectID, client mqtt.Client) bool {
+	var message string
+	var code string
+	var returnVal bool
+	appointment := &schemas.Appointment{}
 
-    col := getAppointmentCollection()
-    filter := bson.M{"_id": id}
-    data := col.FindOne(context.TODO(), filter)
-    result, err := col.DeleteOne(context.TODO(), filter)
+	col := getAppointmentCollection()
+	filter := bson.M{"_id": id}
+	data := col.FindOne(context.TODO(), filter)
+	result, err := col.DeleteOne(context.TODO(), filter)
 
-    if err != nil {
-        panic(err)
-    }
-    
-    if result.DeletedCount == 1{
+	if err != nil {
+		panic(err)
+	}
 
-        code = "200"
+	if result.DeletedCount == 1 {
 
-        data.Decode(appointment)
+		code = "200"
 
-        availableTime := schemas.AvailableTime{
-            Dentist_id: appointment.ID, 
-            Start_time: appointment.Start_time,
-            End_time: appointment.End_time,
-        }
+		data.Decode(appointment)
 
-        jsonData, err := json.Marshal(availableTime)
+		availableTime := schemas.AvailableTime{
+			Dentist_id: appointment.ID,
+			Start_time: appointment.Start_time,
+			End_time:   appointment.End_time,
+		}
 
-        if err != nil {
-            panic(err)
-        }
+		jsonData, err := json.Marshal(availableTime)
 
-        message = string(jsonData)
+		if err != nil {
+			panic(err)
+		}
 
-        fmt.Printf("Här")
-        client.Publish("appointmentservice/internal/migrate", 0, false, message)
+		message = string(jsonData)
 
-        message = `{"message": "Appointment Canceled"`
-        message = AddCodeStringJson(message, code)
-        
-        returnVal = true
+		fmt.Printf("Här")
+		client.Publish("appointmentservice/internal/migrate", 0, false, message)
 
-    } else {
+		message = `{"message": "Appointment Canceled"`
+		message = AddCodeStringJson(message, code)
 
-        code = "404"
-        message = `{"message": "Appointment not found"`
-        message = AddCodeStringJson(message, code)
+		returnVal = true
 
-        returnVal = false
-    }
+	} else {
 
-    client.Publish("grp20/res/appointment/delete", 0, false, message)
-    return returnVal
+		code = "404"
+		message = `{"message": "Appointment not found"`
+		message = AddCodeStringJson(message, code)
+
+		returnVal = false
+	}
+
+	client.Publish("grp20/res/appointment/delete", 0, false, message)
+	return returnVal
 }
 
+func exist(payload schemas.Appointment) bool {
+	col := getAppointmentCollection()
+
+	filter := bson.M{
+		"Dentist_id": payload.Dentist_id,
+		"Start_time": payload.Start_time,
+		"End_time":   payload.End_time,
+	}
+
+	count, err := col.CountDocuments(context.Background(), filter)
+	if err != nil {
+		return false
+	}
+
+	return count > 0
+}
 
 func getAppointmentCollection() *mongo.Collection {
-    col := database.Database.Collection("Appointments")
-    return col
+	col := database.Database.Collection("Appointments")
+	return col
 }
