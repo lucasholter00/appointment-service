86a07921c73cd795cdedfea3d6feed5d3ac0bf04
Lucas Holter <holter@chalmers.se>
2024-01-07 21:45:26 +0100

    #26 (Ongoing) Change dockerfile

--- mqtt/client.go
+++ mqtt/client.go
@@ -56,6 +56,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 }
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+    fmt.Println("Yeehaw")
 	fmt.Println("MQTT client is connected")
 	controllers.InitialiseAppointment(client)
 	controllers.InitializeAvailableTimes(client)
